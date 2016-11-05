f8fa81749bdc2136200c1a90fb8f5eb6180ddd65
Ninja-Koala <felix.potthast@student.uni-siegen.de>
2016-11-05 21:22:21 +0100

    💩

--- scripts/funke.gd
+++ scripts/funke.gd
@@ -4,8 +4,11 @@ var funk_send
 
 func _fixed_process(delta):
 	if not get_overlapping_bodies().empty():
-		if(Input.is_action_just_pressed("use")):
-			funk_send=true
+		for body in get_overlapping_bodies():
+			if not playing and body.is_in_group("player"):
+				body.show_hint("Send out important data")
+				if(Input.is_action_just_pressed("use")):
+					funk_send=true
 
 func _ready():
 	funk_send=false
