e0686bddae35d2c4ef13d7e5cd81f330340d29ed
MartiniMoe <martini.moe@cheex.de>
2016-11-05 16:15:25 +0100

    add particles

--- scripts/particles.gd
+++ scripts/particles.gd
@@ -1,9 +1,11 @@
 extends Particles2D
 
+export var angle = 0
 
 func _fixed_process(delta):
 	var falling_dir = get_node("/root/World").falling_vector
-	set_param(0, rad2deg(falling_dir.angle()))
+	set_param(0, rad2deg(falling_dir.angle()) + angle)
+	set_param(5, rad2deg(falling_dir.angle()))
 
 
 func _ready():
