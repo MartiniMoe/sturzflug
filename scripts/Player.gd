8278b2d14dcd24ef103b1daf97577158fd7194ef
MartiniMoe <martini.moe@cheex.de>
2016-11-05 15:13:47 +0100

    add player state machine

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -16,6 +16,11 @@ var anim
 var madness = 0
 var madness_mod = 1
 
+var player_state = STATE_IDLE
+const STATE_IDLE = 0
+const STATE_WALKING = 1
+const STATE_JUMPING = 2
+
 
 func _fixed_process(delta):
 	#increment counters
@@ -23,6 +28,7 @@ func _fixed_process(delta):
 	
 	movement(delta)
 	handle_madness(delta)
+	animate()
 
 
 func handle_madness(delta):
@@ -47,25 +53,29 @@ func movement(delta):
 	if (Input.is_action_pressed("move_left")):
 		target_speed += -1
 		set_scale(Vector2(-1, 1))
-		if !anim.is_playing() || anim.get_current_animation() != "walk":
-			anim.play("walk")
+		player_state = STATE_WALKING
 	elif (Input.is_action_pressed("move_right")):
 		target_speed +=  1
 		set_scale(Vector2(1, 1))
-		if !anim.is_playing() || anim.get_current_animation() != "walk":
-			anim.play("walk")
+		player_state = STATE_WALKING
+	elif (on_floor and Input.is_action_just_pressed("jump")):
+		linear_vel.y=-JUMP_SPEED
+		player_state = STATE_JUMPING
 	else:
-		if (!anim.is_playing() || anim.get_current_animation() != "idle") && anim.get_current_animation() != "jump":
-			anim.play("idle")
+		player_state = STATE_IDLE
 	
 	target_speed *= WALK_SPEED
 	linear_vel.x = lerp( linear_vel.x, target_speed, 0.1 )
-	
-	# Jumping
-	if (on_floor and Input.is_action_just_pressed("jump")):
-		linear_vel.y=-JUMP_SPEED
-		#if !anim.is_playing() || anim.get_current_animation() != "jump":
-		#	anim.play("jump")
+
+
+func animate():
+	var current_anim = anim.get_current_animation()
+	if player_state == STATE_IDLE && current_anim != "idle" && on_floor:
+		anim.play("idle")
+	elif player_state == STATE_WALKING && current_anim != "walk":
+		anim.play("walk")
+	elif player_state == STATE_JUMPING && current_anim != "jump":
+		anim.play("jump")
 
 
 func _ready():
