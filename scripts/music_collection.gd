0575973acbf3a2de5ec144ed7758fdd4be74d5e4
Ninja-Koala <felix.potthast@student.uni-siegen.de>
2016-11-05 20:28:13 +0100

    Embed music

--- scripts/music_collection.gd
+++ scripts/music_collection.gd
@@ -4,22 +4,31 @@ var music_played
 
 var playing
 
-var title_list=["foo","bar","baz"]
-var title_good=[true,false,false]
+var title_list=["grind.ogg", "ambient1.ogg", "ambient2.ogg"]
+var title_good=[false,true,true]
+var title_objects
+var music_player
+var player
 
 func _fixed_process(delta):
 	if not get_overlapping_bodies().empty():
 		ind=randi()%title_list.len()
 		title=title_list[ind]
 		good=title_good[ind]
-		if(not playing and Input.is_action_just_pressed("ui_accept")):
-			get_node("music_player").play(title)
+		if(not playing and Input.is_action_just_pressed("use")):
+			music_player.set_stream(title_objects[ind])
+			music_player.play()
 			if good:
-				get_node("/root/World/Player").madness_mod-=1
+				player.madness_mod-=1
 			else:
-				get_node("/root/World/Player").madness_mod+=1
+				player.madness_mod+=1
 			playing=true
 
 func _ready():
+	music_player=get_node("music_player")
+	player=get_node("/root/World/Player")
+	title_objects=[]
+	for title in title_list:
+		title_objects.append(load(title))
 	playing=false
 	set_fixed_process(true)
