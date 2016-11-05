0300a5946252ff9dd939bdc750fe641ec2aa30c5
Ninja-Koala <felix.potthast@student.uni-siegen.de>
2016-11-05 22:44:11 +0100

    Add med-station

---
+++ scripts/med-station.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+var pill_taken
+var player
+
+func _fixed_process(delta):
+	if not get_overlapping_bodies().empty():
+		for body in get_overlapping_bodies():
+			if not pill_taken and body.is_in_group("player"):
+				body.show_hint("Take a pill")
+				if(Input.is_action_just_pressed("use")):
+					pill_taken=true
+					if irand()%2==0:
+						player.madness_mod-=1
+					else:
+						player.madness_mod+=1
+
+func _ready():
+	pill_taken=false
+	player=get_node("/root/World/Player")
+	set_fixed_process(true)
