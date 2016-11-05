c06ba91e61175b68b095f9b5850080327a388234
Ninja-Koala <felix.potthast@student.uni-siegen.de>
2016-11-05 15:12:13 +0100

    Add Alarm Sound

--- scripts/Alarm.gd
+++ scripts/Alarm.gd
@@ -1,14 +1,17 @@
 extends AnimatedSprite
 
 var active
+var alarm_player
 
 func _ready():
 	active=true
 	get_node("/root/World/Player").madness_mod+=1
+	alarm_player=get_node("Alarm_Player")
+	alarm_player.play("alarm")
 	
 func stop_alarm():
 	active=false
 	get_node("/root/World/Player").madness_mod-=1
 	stop()
 	set_frame(0)
-	get_node("Alarm_Player").stop_all()
+	alarm_player.stop_all()
