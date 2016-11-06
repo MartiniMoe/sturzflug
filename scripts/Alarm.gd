09c17f73f96083f575b9b048293cc5ed266b2371
Ninja-Koala <felix.potthast@student.uni-siegen.de>
2016-11-06 12:09:48 +0100

    Randomly start alarm again

--- scripts/Alarm.gd
+++ scripts/Alarm.gd
@@ -4,8 +4,11 @@ var alarm_player
 
 func _ready():
 	alarm_player=get_node("alarm_blink_player")
-	alarm_player.play("alarm_blink")
+	start_alarm()
 
 func stop_alarm():
 	alarm_player.stop_all()
 	alarm_player.seek(0.5,true)
+
+func start_alarm():
+	alarm_player.play("alarm_blink")
--- scripts/Terminal.gd
+++ scripts/Terminal.gd
@@ -3,23 +3,34 @@ extends Area2D
 var alarms
 var alarm_player
 var active
+var time
 
 func _fixed_process(delta):
-	if not get_overlapping_bodies().empty():
-		for body in get_overlapping_bodies():
-			if active and body.is_in_group("player"):
-				body.show_hint("Use to deactivate alarm")
-				if Input.is_action_just_pressed("use"):
-					for alarm in alarms:
-						alarm.stop_alarm()
+	time+=delta
+	if active:
+		if not get_overlapping_bodies().empty():
+			for body in get_overlapping_bodies():
+				if body.is_in_group("player"):
+					body.show_hint("Use to deactivate alarm")
+					if Input.is_action_just_pressed("use"):
+						for alarm in alarms:
+							alarm.stop_alarm()
 						alarm_player.stop_all()
-					get_node("/root/World/Player").alter_madness_mod(-1)
-					active=false
+						get_node("/root/World/Player").alter_madness_mod(-1)
+						active=false
+						time=0
+	elif time >= 30+randf()*10:
+		for alarm in alarms:
+			alarm.start_alarm()
+		alarm_player.play("alarm")
+		active=true
+		get_node("/root/World/Player").alter_madness_mod(1)
 
 
 func _ready():
-	#get_node("/root/World/Player").alter_madness_mod(+1)
+	get_node("/root/World/Player").alter_madness_mod(1)
 	active=true
+	time=0
 	alarms=get_tree().get_nodes_in_group("alarm")
 	alarm_player=get_node("Alarm_Player")
 	alarm_player.play("alarm")
